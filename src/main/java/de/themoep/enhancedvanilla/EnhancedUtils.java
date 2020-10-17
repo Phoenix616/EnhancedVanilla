5097dcff63a7e65f6d7c3171e72a06aff5a0da1a
Phoenix616 <max@themoep.de>
2020-10-17 16:33:01 +0100

    Relicense under GPLv3

--- src/main/java/de/themoep/enhancedvanilla/EnhancedUtils.java
+++ src/main/java/de/themoep/enhancedvanilla/EnhancedUtils.java
@@ -1,5 +1,23 @@
 package de.themoep.enhancedvanilla;
 
+/*
+ * EnhancedVanilla
+ * Copyright (c) 2020 Max Lee aka Phoenix616 (max@themoep.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import org.apache.commons.lang.Validate;
 import org.bukkit.Material;
 import org.bukkit.Tag;
@@ -24,22 +42,6 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
-/**
- * EnhancedVanilla
- * Copyright (C) 2016 Max Lee (https://github.com/Phoenix616/)
- * <p/>
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Mozilla Public License as published by
- * the Mozilla Foundation, version 2.
- * <p/>
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Mozilla Public License v2.0 for more details.
- * <p/>
- * You should have received a copy of the Mozilla Public License v2.0
- * along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
- */
 public class EnhancedUtils {
 
     private static final Random RANDOM = new Random();
--- src/main/java/de/themoep/enhancedvanilla/mechanics/ChickenShearing.java
+++ src/main/java/de/themoep/enhancedvanilla/mechanics/ChickenShearing.java
@@ -1,19 +1,21 @@
 package de.themoep.enhancedvanilla.mechanics;
 
 /*
- * Copyright 2017 Max Lee (https://github.com/Phoenix616/)
- * 
+ * EnhancedVanilla
+ * Copyright (c) 2020 Max Lee aka Phoenix616 (max@themoep.de)
+ *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Mozilla Public License as published by
- * the Mozilla Foundation, version 2.
- * 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Mozilla Public License v2.0 for more details.
- * 
- * You should have received a copy of the Mozilla Public License v2.0
- * along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 import de.themoep.enhancedvanilla.EnhancedUtils;
